8b122a081113332c83a17048021df1660c770e61
Deratiseur <53832959+Deratiseur@users.noreply.github.com>
2026-02-02 04:33:40 +0100

    Add files via upload

--- Derats_Kribinn/D/pxkgilbe.d
+++ Derats_Kribinn/D/pxkgilbe.d
@@ -30,15 +30,15 @@ END
 
 IF ~NumTimesTalkedToGT(0)~ THEN presentation2	SAY @562
 	IF ~~ THEN EXIT
-	IF ~PartyGoldGT(79999)~ THEN DO ~TakePartyGold(80000) DestroyGold(80000) SetGlobal("pxkgilber","GLOBAL",3)~ REPLY @563 GOTO deal1
+	IF ~PartyGoldGT(39999)~ THEN DO ~TakePartyGold(40000) DestroyGold(40000) SetGlobal("pxkgilber","GLOBAL",3)~ REPLY @563 GOTO deal1
 END
 
 IF ~~ THEN prix1	SAY @508
-	IF ~PartyGoldGT(79999)~ THEN DO ~TakePartyGold(80000) DestroyGold(80000)~ REPLY @509 GOTO deal1
-	IF ~PartyGoldGT(69999) CheckStatGT(LastTalkedToBy,12,CHR)~ THEN DO ~TakePartyGold(70000) DestroyGold(70000)~ REPLY @510 GOTO ok11
-	IF ~PartyGoldGT(69999) CheckStatLT(LastTalkedToBy,13,CHR)~ THEN REPLY @510 GOTO nego11
-	IF ~PartyGoldGT(59999) CheckStatGT(LastTalkedToBy,16,CHR)~ THEN DO ~TakePartyGold(60000) DestroyGold(60000)~ REPLY @511 GOTO ok12
-	IF ~PartyGoldGT(59999) CheckStatLT(LastTalkedToBy,17,CHR)~ THEN REPLY @511 GOTO nego12
+	IF ~PartyGoldGT(39999)~ THEN DO ~TakePartyGold(40000) DestroyGold(40000)~ REPLY @509 GOTO deal1
+	IF ~PartyGoldGT(29999) CheckStatGT(LastTalkedToBy,12,CHR)~ THEN DO ~TakePartyGold(30000) DestroyGold(30000)~ REPLY @510 GOTO ok11
+	IF ~PartyGoldGT(29999) CheckStatLT(LastTalkedToBy,13,CHR)~ THEN REPLY @510 GOTO nego11
+	IF ~PartyGoldGT(19999) CheckStatGT(LastTalkedToBy,18,CHR)~ THEN DO ~TakePartyGold(20000) DestroyGold(20000)~ REPLY @511 GOTO ok12
+	IF ~PartyGoldGT(19999) CheckStatLT(LastTalkedToBy,19,CHR)~ THEN REPLY @511 GOTO nego12
 	IF ~~ THEN REPLY @512 EXIT
 END
 
@@ -63,15 +63,15 @@ IF ~~ THEN ok22	SAY @557
 END
 
 IF ~~ THEN nego11	SAY @514
-	IF ~PartyGoldGT(79999)~ THEN DO ~TakePartyGold(80000) DestroyGold(80000)~ REPLY @515 GOTO deal1
+	IF ~PartyGoldGT(39999)~ THEN DO ~TakePartyGold(40000) DestroyGold(40000)~ REPLY @515 GOTO deal1
 	IF ~~ THEN REPLY @516 EXIT
 END
 
 IF ~~ THEN nego12	SAY @518
-	IF ~PartyGoldGT(79999)~ THEN DO ~TakePartyGold(80000) DestroyGold(80000)~ REPLY @519 GOTO deal1
-	IF ~PartyGoldGT(74999)CheckStatGT(LastTalkedToBy,12,CHR)~ THEN DO ~TakePartyGold(75000) DestroyGold(75000)~ REPLY @520 GOTO deal1
-	IF ~PartyGoldGT(74999)CheckStatLT(LastTalkedToBy,13,CHR)~ THEN REPLY @520 GOTO nook12
-	IF ~PartyGoldGT(64999)~ THEN REPLY @521 GOTO nook11
+	IF ~PartyGoldGT(39999)~ THEN DO ~TakePartyGold(40000) DestroyGold(40000)~ REPLY @519 GOTO deal1
+	IF ~PartyGoldGT(34999)CheckStatGT(LastTalkedToBy,12,CHR)~ THEN DO ~TakePartyGold(35000) DestroyGold(35000)~ REPLY @520 GOTO deal1
+	IF ~PartyGoldGT(34999)CheckStatLT(LastTalkedToBy,13,CHR)~ THEN REPLY @520 GOTO nook12
+	IF ~PartyGoldGT(24999)~ THEN REPLY @521 GOTO nook11
 	IF ~~ THEN REPLY @522 EXIT
 END
 
@@ -104,11 +104,11 @@ IF ~~ THEN nook24	SAY @556
 END
 
 IF ~~ THEN semi	SAY @526
-	IF ~PartyGoldGT(79999)~ THEN DO ~TakePartyGold(80000) DestroyGold(80000)~ REPLY @509 GOTO deal1
-	IF ~PartyGoldGT(69999) CheckStatGT(LastTalkedToBy,12,CHR)~ THEN DO ~TakePartyGold(70000) DestroyGold(70000)~ REPLY @527 GOTO ok11
-	IF ~PartyGoldGT(69999) CheckStatLT(LastTalkedToBy,13,CHR)~ THEN REPLY @527 GOTO nego11
-	IF ~PartyGoldGT(54999) CheckStatGT(LastTalkedToBy,18,CHR)~ THEN DO ~TakePartyGold(55000) DestroyGold(55000)~ REPLY @528 GOTO ok13
-	IF ~PartyGoldGT(54999) CheckStatLT(LastTalkedToBy,19,CHR)~ THEN REPLY @528 GOTO nook13
+	IF ~PartyGoldGT(39999)~ THEN DO ~TakePartyGold(40000) DestroyGold(40000)~ REPLY @509 GOTO deal1
+	IF ~PartyGoldGT(29999) CheckStatGT(LastTalkedToBy,12,CHR)~ THEN DO ~TakePartyGold(30000) DestroyGold(30000)~ REPLY @527 GOTO ok11
+	IF ~PartyGoldGT(29999) CheckStatLT(LastTalkedToBy,13,CHR)~ THEN REPLY @527 GOTO nego11
+	IF ~PartyGoldGT(16999) CheckStatGT(LastTalkedToBy,18,CHR)~ THEN DO ~TakePartyGold(17000) DestroyGold(17000)~ REPLY @528 GOTO ok13
+	IF ~PartyGoldGT(16999) CheckStatLT(LastTalkedToBy,19,CHR)~ THEN REPLY @528 GOTO nook13
 	IF ~~ THEN REPLY @529 EXIT
 END
 
@@ -124,13 +124,13 @@ IF ~~ THEN krotann	SAY @535
 END
 
 IF ~~ THEN nego21	SAY @539
-	IF ~PartyGoldLT(1000000)~ THEN REPLY @564 DO ~SetGlobal("pxkgilber","GLOBAL",1) EscapeArea()~ EXIT
-	IF ~PartyGoldGT(999999)~ THEN REPLY @540 GOTO nook21
-	IF ~PartyGoldGT(199999) CheckStatGT(LastTalkedToBy,14,CHR)~ THEN REPLY @541 GOTO nego22
-	IF ~PartyGoldGT(199999) CheckStatLT(LastTalkedToBy,15,CHR)~ THEN REPLY @541 GOTO nook22
-	IF ~PartyGoldGT(299999) CheckStatGT(LastTalkedToBy,16,CHR)~ THEN DO ~TakePartyGold(300000) DestroyGold(300000)~ REPLY @542 GOTO ok21
-	IF ~PartyGoldGT(299999) CheckStatLT(LastTalkedToBy,17,CHR)~ THEN REPLY @542 GOTO nook24
-	IF ~PartyGoldGT(399999)~ THEN DO ~TakePartyGold(400000) DestroyGold(400000)~ REPLY @543 GOTO deal2
+	IF ~PartyGoldLT(50000)~ THEN REPLY @564 DO ~SetGlobal("pxkgilber","GLOBAL",1) EscapeArea()~ EXIT
+	IF ~PartyGoldGT(49999)~ THEN REPLY @540 GOTO nook21
+	IF ~PartyGoldGT(99999) CheckStatGT(LastTalkedToBy,14,CHR)~ THEN REPLY @541 GOTO nego22
+	IF ~PartyGoldGT(99999) CheckStatLT(LastTalkedToBy,15,CHR)~ THEN REPLY @541 GOTO nook22
+	IF ~PartyGoldGT(149999) CheckStatGT(LastTalkedToBy,16,CHR)~ THEN DO ~TakePartyGold(300000) DestroyGold(300000)~ REPLY @542 GOTO ok21
+	IF ~PartyGoldGT(149999) CheckStatLT(LastTalkedToBy,17,CHR)~ THEN REPLY @542 GOTO nook24
+	IF ~PartyGoldGT(199999)~ THEN DO ~TakePartyGold(200000) DestroyGold(200000)~ REPLY @543 GOTO deal2
 END
 
 IF ~~ THEN obsidienne	SAY @551
@@ -138,17 +138,17 @@ IF ~~ THEN obsidienne	SAY @551
 END
 
 IF ~~ THEN obsidienne2	SAY @553
-	IF ~PartyGoldGT(399999)~ THEN DO ~TakePartyGold(400000) DestroyGold(400000)~ REPLY @548 GOTO deal2
-	IF ~PartyGoldGT(299999) CheckStatGT(LastTalkedToBy,16,CHR)~ THEN DO ~TakePartyGold(300000) DestroyGold(300000)~ REPLY @554 GOTO ok21
-	IF ~PartyGoldGT(299999) CheckStatLT(LastTalkedToBy,17,CHR)~ THEN REPLY @554 GOTO nook23
-	IF ~PartyGoldGT(249999) CheckStatGT(LastTalkedToBy,15,INT) CheckStatLT(LastTalkedToBy,17,CHR)~ THEN REPLY @555 GOTO nook24
-	IF ~PartyGoldGT(249999) CheckStatGT(LastTalkedToBy,15,INT) CheckStatGT(LastTalkedToBy,16,CHR)~ THEN DO ~TakePartyGold(250000) DestroyGold(250000)~ REPLY @555 GOTO ok22
+	IF ~PartyGoldGT(199999)~ THEN DO ~TakePartyGold(200000) DestroyGold(200000)~ REPLY @548 GOTO deal2
+	IF ~PartyGoldGT(149999) CheckStatGT(LastTalkedToBy,16,CHR)~ THEN DO ~TakePartyGold(150000) DestroyGold(150000)~ REPLY @554 GOTO ok21
+	IF ~PartyGoldGT(149999) CheckStatLT(LastTalkedToBy,17,CHR)~ THEN REPLY @554 GOTO nook23
+	IF ~PartyGoldGT(129999) CheckStatGT(LastTalkedToBy,15,INT) CheckStatLT(LastTalkedToBy,17,CHR)~ THEN REPLY @555 GOTO nook24
+	IF ~PartyGoldGT(129999) CheckStatGT(LastTalkedToBy,15,INT) CheckStatGT(LastTalkedToBy,16,CHR)~ THEN DO ~TakePartyGold(130000) DestroyGold(130000)~ REPLY @555 GOTO ok22
 END
 
 IF ~~ THEN nego22	SAY @546
-	IF ~PartyGoldGT(299999) CheckStatGT(LastTalkedToBy,16,CHR)~ THEN DO ~TakePartyGold(300000) DestroyGold(300000)~ REPLY @547 GOTO ok21
-	IF ~PartyGoldGT(299999) CheckStatLT(LastTalkedToBy,17,CHR)~ THEN REPLY @547 GOTO nook23
-	IF ~PartyGoldGT(399999)~ THEN DO ~TakePartyGold(400000) DestroyGold(400000)~ REPLY @548 GOTO deal2
+	IF ~PartyGoldGT(149999) CheckStatGT(LastTalkedToBy,16,CHR)~ THEN DO ~TakePartyGold(150000) DestroyGold(150000)~ REPLY @547 GOTO ok21
+	IF ~PartyGoldGT(149999) CheckStatLT(LastTalkedToBy,17,CHR)~ THEN REPLY @547 GOTO nook23
+	IF ~PartyGoldGT(199999)~ THEN DO ~TakePartyGold(200000) DestroyGold(200000)~ REPLY @548 GOTO deal2
 	IF ~~ THEN REPLY @561 EXIT
 END
 
